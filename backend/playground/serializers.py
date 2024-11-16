058a6443e647d45a62ddef59ff47abf4ab985041
Camilly Shrestha <cshrestha2@springfieldcollege.edu>
2024-11-16 12:01:42 -0500

    Removed the django

--- backend/playground/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import CheckIn
-
-class CheckInSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CheckIn
-        fields = '__all__'
--- backend/playground/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import checkin, get_history
-
-urlpatterns = [
-    path('checkin/', checkin),
-    path('history/', get_history),
-]
